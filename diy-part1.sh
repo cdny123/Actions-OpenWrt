e4001634c555c8441901c9fb6ba7323572031516
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-01-15 13:06:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,11 +20,9 @@ echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.d
 echo 'src-git openwrt-package https://github.com/cdny123/openwrt-package.git' >>feeds.conf.default
 
 # 添加 APP 插件
-echo "克隆 APP 插件..."
 git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt      # chatgpt-web
 git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-app-kucat   # kucat主题
 git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart   # iStoreOS-web
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky      # luci-app-lucky 端口转发
 
-echo "所有操作完成。"
-
+cd openwrt && ./scripts/feeds update -a
